f26d221b3ae8fa5adef3d6903f9285d3a31b369a
ProsperChihimba <prosperchihimba@gmail.com>
2022-05-01 17:21:04 +0300

    Add authorization

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StreamChat } from 'stream-chat';
 import { OverlayProvider, Chat, ChannelList, Channel, MessageList, MessageInput } from 'stream-chat-expo';
 import { Text } from 'react-native';
 
+import AuthContext from "./contexts/Authentication";
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -16,28 +17,11 @@ export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  const [isReady, setIsReady] = useState(false);
+  const [userId, setUserId] = useState("");
+
   const [selectedChannel, setSelectedChannel] = useState<any>(null);
 
   useEffect(() => {
-    const connectUser = async () => { 
-      await client.connectUser(
-        {
-          id: 'proc',
-          name: 'prosper',
-          image: 'https://avatars.githubusercontent.com/u/72831371?v=4',
-        },
-        client.devToken('proc'),
-      ); 
-
-      //channel
-      const channel = client.channel("messaging", "GutaChannel", { name: 'Guta' });
-      await channel.watch();
-      setIsReady(true);
-    };
-
-    connectUser();
-
     return () => client.disconnectUser();
   }, []);
 
@@ -45,29 +29,33 @@ export default function App() {
     setSelectedChannel(channel);
   }
 
-  if (!isLoadingComplete || !isReady) {
+  if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <OverlayProvider>
-          <Chat client={client} >
-            {/* <Navigation colorScheme={colorScheme} /> */}
-            {selectedChannel ? (
-              <Channel channel={selectedChannel}>
-                <Text
-                  style={{ margin: 50 }}
-                  onPress= {() => setSelectedChannel(null)}
-                >Back</Text>
-                <MessageList />
-                <MessageInput />
-              </Channel>
-            ) : (
-                <ChannelList onSelect={onChannelPressed}/>
-            )}
-          </Chat>
-        </OverlayProvider>
-        <StatusBar />
+        <AuthContext.Provider value={{ userId, setUserId }}>
+          <OverlayProvider>
+            <Chat client={client}>
+              <Navigation colorScheme={colorScheme} />
+            </Chat>
+            {/* <Chat client={client} >
+              {selectedChannel ? (
+                <Channel channel={selectedChannel}>
+                  <Text
+                    style={{ margin: 50 }}
+                    onPress= {() => setSelectedChannel(null)}
+                  >Back</Text>
+                  <MessageList />
+                  <MessageInput />
+                </Channel>
+              ) : (
+                  <ChannelList onSelect={onChannelPressed}/>
+              )}
+            </Chat> */}
+          </OverlayProvider>
+          <StatusBar />
+        </AuthContext.Provider>
       </SafeAreaProvider>
     );
   }
